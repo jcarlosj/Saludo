d95a0bb2be37fc05063b781bf500128f0364d955
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-07-30 12:32:13 -0500

    Creación de "Activity" para el saludo al usuario.

---
+++ app/src/main/java/co/jce/sena/saludo/SaludoActivity.java
@@ -0,0 +1,16 @@
+package co.jce.sena.saludo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/*
+ * Created by jce on 30/07/15.
+ */
+public class SaludoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super .onCreate( savedInstanceState );
+        setContentView( R .layout .activity_saludo );
+    }
+}
